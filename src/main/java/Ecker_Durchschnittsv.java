c610c6e5d5b3fb402d75b1aa9ce9615ebb72e8d3
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-12 14:24:36 +0100

    finish code

--- src/main/java/Ecker_Durchschnittsv.java
+++ src/main/java/Ecker_Durchschnittsv.java
@@ -7,8 +7,10 @@ public class Ecker_Durchschnittsv {
 
         double gesamtstrecke;
         double leavetime;
+        double meettime;
         double vA;
         double vB;
+        double meetpoint;
 
         System.out.print("Gesammtstreck: ");
         gesamtstrecke = scanner.nextDouble();
@@ -28,6 +30,10 @@ public class Ecker_Durchschnittsv {
         vB = scanner.nextDouble();
         System.out.println();
 
+        meettime = leavetime + ((gesamtstrecke / (vA + vB)) / 100 * 60);
+        meetpoint = (gesamtstrecke / (vA + vB) * vA);
 
+        System.out.printf("Sie treffen sich um %.2f Uhr \n" , meettime);
+        System.out.printf("Sie trefen sich %.1f Kilometer vom Punkt a entfernt wieder" , meetpoint);
     }
 }
